8bf04057e42bd3dc7f07651964f3828d9fd0e115
Pedro Henrique <phrsilva.adv@gmail.com>
2025-01-21 14:03:18 -0300

    teste do user

---
+++ src/service/UserCreateService.test.js
@@ -0,0 +1,9 @@
+const userCreateService = require('./UserCreateService');
+
+it("Usuário deve ser criado", () => {
+    const user = {
+        name: "Rauner",
+        email: "rauner@raunerlimpezas.com.br",
+        password: "123456"
+    }
+})
